f66e81122a389db3ee677448c3dffd642eb7cea3
DanielRickyA <danielrickya1404@gmail.com>
2024-10-21 10:37:57 +0700

    add new file and add new image and font

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wisatabandung/detail_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,77 +14,6 @@ class MainApp extends StatelessWidget {
       title: "Wisata Bandung",
       theme: ThemeData(useMaterial3: false),
       home: const DetailScreen(),
-    );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.only(top: 16.0),
-              child: const Text(
-                "Farm house Lembang",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 16.0),
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.calendar_today),
-                      SizedBox(
-                        height: 8.0,
-                      ),
-                      Text("Open Everyday"),
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.schedule),
-                      SizedBox(
-                        height: 8.0,
-                      ),
-                      Text("09:00 - 20:00")
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.paid_sharp),
-                      SizedBox(
-                        height: 8.0,
-                      ),
-                      Text("09:00 - 20:00")
-                    ],
-                  )
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              child: const Text(
-                'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16.0),
-              ),
-            )
-          ],
-        ),
-      ),
     );
   }
 }
